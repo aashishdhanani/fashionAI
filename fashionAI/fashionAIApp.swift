1d50b441e9b322f6180b9aeff04958e06ef5213d
aashishdhanani <aashishd2004@gmail.com>
2024-06-17 11:36:46 -0500

    Initial Commit

---
+++ fashionAI/fashionAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  fashionAIApp.swift
+//  fashionAI
+//
+//  Created by Aashish Dhanani on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct fashionAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
